f3eff7fb76984d8f8e0599eaa9ba7b193bbf5294
David <david.minxiao.li@gmail.com>
2020-12-05 16:36:19 -0800

    Add express middleware defaults

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,15 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
 const app: express.Application = express();
+
+app.use(logger('dev'));
+app.enable('trust proxy');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Hello ME!');
